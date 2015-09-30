fb197c741c9835cd1aacb4a76dc9f280cfd60d71
somename <someemail>
2015-09-30 15:38:41 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -17,7 +17,7 @@ public class TestSudokuVerifier {
 	public void rightSolutionTest() {
 		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		int verify = SudokuVerifier.verify (candidateSolution);
-		assertEquals(1, verify);
+		assertEquals(1, SudokuVerifier.verify (candidateSolution));
 	}
 
 }
