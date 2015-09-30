4b3049dbb2cf453962dd0d526546516b1eb16c1e
somename <someemail>
2015-09-30 16:31:28 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -31,9 +31,15 @@ public class TestSudokuVerifier {
 	}
 	
 	@Test
-	public void isNotPositiveTest() {
+	public void noSameInRowTest() {
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		assertEquals(0, SudokuVerifier.positive (candidateSolution));
+	}
+	
+	@Test
+	public void sameInRowTest() {
 		String candidateSolution = "4417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-		assertEquals(-3, SudokuVerifier.positive (candidateSolution));
+		assertEquals(-3, SudokuVerifier.row (candidateSolution));
 	}
 	
 }
