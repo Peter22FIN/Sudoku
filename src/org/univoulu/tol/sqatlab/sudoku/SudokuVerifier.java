c7323550ca83f28e8f90af42c23e29fa5e4c11ee
somename <someemail>
2015-09-30 16:33:04 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -56,6 +56,7 @@ public class SudokuVerifier {
 			if (candidateSolution.charAt(i)==56) eight++;
 			if (candidateSolution.charAt(i)==57) nine++;
 			if (one>1 || two>1 || three>1 || four>1 || five>1 || six>1 || seven>1 || eight>1 || nine>1) return -3;
+			j++;
 		}
 		return 0;
 	}
